50540eaec68436013bc88362e316a9d14c98ac2b
Mahmoud <barghoutmahmoud2@gmail.com>
2022-11-21 14:27:34 +0200

    completed the basic functionality. added the search box for user to enter desired location. added a display div in html. in js added asyn function to request and bring back the weather data in json format. added another function that takes the json and calls a result display function with it. added the result display function that takes certain data out of the json and makes paragraph elements with them and adds them to the display div . added the function that does a gif search based on the weather status and returns a url that is used in the display function to display that gif.

--- WthrApp.js
+++ WthrApp.js
@@ -1,8 +1,56 @@
+const requestBttn = document.querySelector(".submit");
+const locationRequested = document.querySelector("input#location");
+const resultDisplay = document.querySelector(".results");
+
 async function requestData(location) {
   const WeatherData = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=6366d25bd31143aecc1784f5317109fd`
   );
-  console.log(WeatherData.json());
+  return WeatherData.json();
 }
 
-requestData('Cairo');
+async function ProcessData(location) {
+  const requestedData = await requestData(location);
+  displayWeatherData(requestedData);
+}
+
+async function searchGif(searchphrase) {
+  const requestGif = await fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=ETioSUOsXxlouIGNp0BMISKogln3K5ec&s=${searchphrase}`
+  );
+  const requestJSON = await requestGif.json();
+  const returnData = await requestJSON.data.images.original.url;
+  //console.log(returnData);
+  return returnData;
+}
+
+async function displayWeatherData(info) {
+  //console.log(info);
+  firstParagraph = document.createElement("p");
+  firstParagraph.textContent = `location: ${info.name}`;
+  resultDisplay.appendChild(firstParagraph);
+  secondParagraph = document.createElement("p");
+  secondParagraph.textContent = `temprature is ${(
+    info.main["temp"] - 273
+  ).toFixed(2)} celsius /  ${((9 / 5) * (info.main["temp"] - 273) + 32).toFixed(
+    2
+  )} fahrenheit`;
+  resultDisplay.appendChild(secondParagraph);
+  thirdParagraph = document.createElement("p");
+  thirdParagraph.textContent = `current weather status is ${info.weather[0]["description"]}`;
+  resultDisplay.appendChild(thirdParagraph);
+  displayGif = document.createElement("img");
+  displayGif.src = await searchGif(info.weather[0]["description"]);
+  resultDisplay.appendChild(displayGif);
+}
+
+function clearDisplay() {
+  while (resultDisplay.firstChild) {
+    resultDisplay.removeChild(resultDisplay.lastChild);
+  }
+}
+
+requestBttn.addEventListener("click", () => {
+  clearDisplay();
+  ProcessData(locationRequested.value);
+});
